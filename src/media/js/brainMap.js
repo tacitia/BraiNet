a5ff693ab9d02a3d884d3e6560cd4d654baae390
ArthurYidi <ayidi@risd.edu>
2012-06-08 19:26:49 -0400

    organized files

---
+++ src/media/js/brainMap.js
@@ -0,0 +1,67 @@
+(function() {
+    brainMap = {
+        // Lazily construct the brain hierarchy from brain region names.
+        root: function(data) {
+            var map = {};
+            function find(name, data, key) {
+                var node = map[name], i;
+                if (!node) {
+                    node = map[name] = data || {name: name, children: []};
+                    if (name.length) {
+                        node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+                        node.parent.children.push(node);
+                        node.key = key;
+                        node.displayName = name.substring(i + 1);
+                        node.children = [];
+                    }
+                }
+                return node;
+            }
+
+
+            data.forEach(function(d) {
+
+                find(d.name, d, d.key);
+            }
+            );
+            return map[""];
+        },
+        // Generate the mapping from [source region name, target region name] to an ID of the associated literature
+        evidence: function(nodes) {
+            var map = {};
+
+            nodes.forEach(function(d) {
+                if (d.links) d.links.forEach(function(i) {
+                    map[d.name, i.name] = i.PMID;
+                });
+            });
+
+            return map;
+        },
+        // Return a list of connections for the given array of brain regions.
+        connections: function(nodes) {
+            var map = {},
+            links = [];
+
+            // Compute a map from name to node.
+            nodes.forEach(function(d) {
+                map[d.name] = d;
+            });
+
+            // Construct links
+            nodes.forEach(function(d) {
+                if (d.links) d.links.forEach(function(i) {
+                    // Debug code: map[i.name] will be undefined if the connectivity data is incorrect
+                    if (map[i.name] == undefined) {
+                        console.log(d.name);
+                        console.log(i.name);
+                    }
+                    else {
+                        links.push({source: map[d.name], target: map[i.name]});
+                    }
+                });
+            });
+            return links;
+        }
+    };
+})();
---
+++ src/media/js/script.js
@@ -0,0 +1,120 @@
+var w = 1440,
+    h = 900,
+    m0,
+    rotate = 0;
+    radius = Math.min(w, h) / 2,
+    color = d3.scale.category20c();
+
+var selectedSources = [];
+var selectedTargets = [];
+
+var splines = [];
+
+var cluster = d3.layout.cluster()
+    .size([360, 960 / 2 - 120])
+    .sort(null)
+    .value(function(d) { return d.size; });
+
+var partition = d3.layout.partition()
+    .sort(null)
+    .size([2 * Math.PI, radius * radius])
+    .value(function(d) { return 1; });
+
+var bundle = d3.layout.bundle();
+
+var line = d3.svg.line.radial()
+    .interpolate("bundle")
+    .tension(.85)
+    .radius(function(d) { return d.y; })
+    .angle(function(d) { return d.x / 180 * Math.PI; });
+
+
+var svg = d3.select("body")
+    .append("svg:svg")
+    .attr("width", 1440)
+    .attr("height", 900)
+    .append("svg:g")
+    .attr("transform", "translate(600,480)");
+
+
+d3.json("../media/data/brainData.txt", function(data) {
+
+    var nodes = cluster.nodes(brainMap.root(data));
+    links = brainMap.connections(nodes);
+    splines = bundle(links);
+    conMap = brainMap.evidence(nodes);
+
+    var path = svg.selectAll("path.link")
+    .data(links)
+    .enter().append("svg:path")
+    .attr("class", function(d) {return "link source-" + d.source.key + " target-" + d.target.key})
+    .attr("d", function(d, i) { return line(splines[i]); })
+    .on("click", click);
+
+    var node = svg.selectAll("g.node")
+    //      .data(nodes.filter(function(n) { return !n.children; }))
+    .data(nodes)
+    .enter()
+    .append("svg:g")
+    .attr("class", "node")
+    .attr("id", function(d) {return "node-" + d.key;})
+    .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; });
+
+    node.append("svg:text")
+    .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
+    .attr("dy", ".31em")
+    .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+    .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
+    .text(function(d) { return d.displayName; })
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout)
+    .on("click", nodeClick);
+
+    node.append("circle")
+    .attr("r", function(d) {return 1});
+});
+
+d3.select(self.frameElement).style("height", "960px");
+
+
+function mouse(e) {
+    return [e.pageX - rx, e.pageY - ry];
+}
+
+function mouseover(d) {
+    svg.selectAll("path.link.target-" + d.key)
+    .classed("target", true)
+    .each(updateNodes("source", true));
+
+    svg.selectAll("path.link.source-" + d.key)
+    .classed("source", true)
+    .each(updateNodes("target", true));
+    console.log(d.name);
+}
+
+function mouseout(d) {
+    svg.selectAll("path.link.source-" + d.key)
+    .classed("source", false)
+    .each(updateNodes("target", false));
+
+    svg.selectAll("path.link.target-" + d.key)
+    .classed("target", false)
+    .each(updateNodes("source", false));
+}
+
+function updateNodes(name, value) {
+    return function(d) {
+        //if (value) this.data.parentNode.appendChild(this);
+        svg.select("#node-" + d.target.key).classed(name, value);
+    };
+}
+
+function click(d) {
+    var source = d.source.name;
+    var target = d.target.name;
+    window.location.href = 'http://www.ncbi.nlm.nih.gov/pubmed?term=' + conMap[source, target];
+}
+
+function nodeClick(d) {
+}
+
