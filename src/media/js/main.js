d6fd8dc07f6ed145502b12db8d80c44c543f2612
tacitia <lillian.g621@gmail.com>
2012-10-25 14:09:14 -0400

    add zoom in functionalities

--- src/media/js/main.js
+++ src/media/js/main.js
@@ -42,41 +42,49 @@ var directionType = {
     "bi": 3
 }
 
+// Misc program control variables
+var mutex = 2;
+
 /*******
     End of data declaration section
 *******/
 
 /*******
-    Data initialization section
+    Data loading section
 *******/
-// Ok, assume that we already have the data for now.
-node_map = {};
-node_map[1] = {key: 1, name: "apple", depth: 0, children:[2,3]};
-node_map[2] = {key: 2, name: "banana", depth: 1, children: [4,5]};
-node_map[3] = {key: 3, name: "plum", depth: 1, children: [6,7]};
-node_map[4] = {key: 4, name: "grape", depth: 2, children: []};
-node_map[5] = {key: 5, name: "watermelon", depth: 2, children: []};
-node_map[6] = {key: 6, name: "orange", depth: 2, children: []};
-node_map[7] = {key: 7, name: "pineapple", depth: 2, children: []};
 
-link_map = {};
-link_map[1] = {key: 1, source: node_map[2], target: node_map[3], paper: [1,2], bams_records: []};
-link_map[2] = {key: 2, source: node_map[4], target: node_map[6]};
-link_map[3] = {key: 3, source: node_map[5], target: node_map[7]};
-link_map[4] = {key: 4, source: node_map[4], target: node_map[3]};
-link_map[5] = {key: 5, source: node_map[5], target: node_map[3]};
-link_map[6] = {key: 6, source: node_map[2], target: node_map[6]};
-link_map[7] = {key: 7, source: node_map[2], target: node_map[7]};
+d3.json("media/data/test_node.json", function (data) {
+    node_map = {};
+    var num_nodes = data.length;
+    for (var i = 0; i < num_nodes; ++i) {
+        var node = data[i];
+        node_map[data[i].key] = node;
+    }
+    mutex -= 1;
+});
 
-node_link_map = {};
-node_link_map["2-3"] = link_map[1];
-node_link_map["4-6"] = link_map[2];
-node_link_map["5-7"] = link_map[3];
-node_link_map["3-4"] = link_map[4];
-node_link_map["3-5"] = link_map[5];
-node_link_map["2-6"] = link_map[6];
-node_link_map["2-7"] = link_map[7];
+d3.json("media/data/test_link.json", function (data) {
+    link_map = {};
+    node_link_map = {}
+    var num_links = data.length;
+    for (var i = 0; i < num_links; ++i) {
+        var raw_link = data[i];
+        var link = {key: raw_link.key, source: node_map[raw_link.sourceKey], 
+                    target: node_map[raw_link.targetKey]};
+        link_map[link.key] = link;
+//        var key_pair = generateKeyForNodeLinkMap(link.source, link.target);
+        var key_pair = link.source.key + "-" + link.target.key;
+        node_link_map[key_pair] = link;
+    }
+    mutex -= 1;
+});
 
+waitForDataLoading();
+
+/*******
+    End of data loading section
+*******/
+/*
 node_neighbor_map = {};
 node_neighbor_map[2] = [{node: node_map[3]}, {node: node_map[6]}, {node: node_map[7]}];
 node_neighbor_map[3] = [{node: node_map[2]}, {node: node_map[4]}, {node: node_map[5]}];
@@ -90,59 +98,58 @@ paper_map[1] = {key: 1, title: "paper 1", url: "http://pubmed"};
 paper_map[2] = {key: 2, title: "paper 2", url: "http://pubmed"};
 
 bams_map = {};
-bams_map[1] = {key: 1, url: "", };
+bams_map[1] = {key: 1, url: ""};
+*/
 
-// Initialize the active nodes to be the highest level ones
-active_data_nodes = [];
-for (var key in node_map) {
-    var curr_node = node_map[key];
-    if (curr_node.depth === 1) {
-        active_data_nodes.push(curr_node);
+function renderCanvas() {
+    // Initialize the active nodes to be the highest level ones
+    active_data_nodes = [];
+    for (var key in node_map) {
+        var curr_node = node_map[key];
+        if (curr_node.depth === 1) {
+            active_data_nodes.push(curr_node);
+        }
     }
+
+    computeArcParameters(active_data_nodes);
+
+    // Initialize the active links according to the active nodes
+    active_data_links = [];
+    for (var key in link_map) {
+        var curr_link = link_map[key];
+        if (curr_link.source.depth === 1 && curr_link.target.depth === 1) {
+            active_data_links.push(curr_link);
+        }
+    }
+
+    // Setup the arc function object
+    arcs = d3.svg.arc()
+             .innerRadius(inner_radius)
+             .outerRadius(outer_radius);
+
+    // Initialize the background svg canvas
+    svg = d3.select("#canvas")
+            .append("svg")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .append('g')
+            .attr("transform", "translate(" + (vis_width  * 2 / 3) + "," + (vis_height / 2) + ")")
+            .append('g');
+
+    // Render the arcs
+    enterNodes();
+    // Render the links
+    enterLinks();
 }
 
-computeArcParameters(active_data_nodes);
-
-// Initialize the active links according to the active nodes
-active_data_links = [];
-for (var key in link_map) {
-    var curr_link = link_map[key];
-    if (curr_link.source.depth === 1 && curr_link.target.depth === 1) {
-        active_data_links.push(curr_link);
+function waitForDataLoading() {
+    if (mutex > 0) {
+        setTimeout(function() {waitForDataLoading();}, 1000);
+    }
+    else {
+        renderCanvas();
     }
 }
-
-/*******
-    End of data initialization section
-*******/
-
-/*******
-    SVG rendering section
-*******/
-// Setup the arc function object
-arcs = d3.svg.arc()
-         .innerRadius(inner_radius)
-         .outerRadius(outer_radius);
-
-// Initialize the background svg canvas
-svg = d3.select("#canvas")
-        .append("svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .append('g')
-        .attr("transform", "translate(" + (vis_width  * 2 / 3) + "," + (vis_height / 2) + ")")
-        .append('g');
-
-// Render the arcs
-enterNodes();
-
-// Render the links
-enterLinks();
-
-
-/*******
-    End of the SVG rendering section
-*******/
 
 
 
--- src/media/js/svg.js
+++ src/media/js/svg.js
@@ -7,59 +7,60 @@
  */
 
 
-function fade(opacity) {
-   return function(g, i) {
-     svg.selectAll("g.chord path")
-         .filter(function(d) {
-           return d.source.index != i && d.target.index != i;
-         })
-       .transition()
-         .style("opacity", opacity);
-   };
-}
-
-
 function expandRegion(d) {
+    // First check the children. If no children, do nothing and return.
     var child_keys = d.children;
     var total_num = child_keys.length;
     if (total_num < 1) {return;}
-
+    
     // Add the sub-regions of the original region that has been chosen to be expanded
     var start_angle = d.startAngle;
     var end_angle = d.endAngle;
-    
     var delta = (end_angle - start_angle) / total_num;
 
-    var active_neighbors = [];
+    // Record neighbors of the node being removed
+    var in_neighbors = [];
+    var out_neighbors = [];
+    var old_link_keys = [];
     var link_length = active_data_links.length;
     for (var i = 0; i < link_length; ++i) {
         var curr_link = active_data_links[i];
-        if (curr_link.source === d) {
-            active_neighbors.push(curr_link.target);
+        if (curr_link.source === d){
+            out_neighbors.push(curr_link.target);
+            old_link_keys.push(curr_link.source.key + "-" + curr_link.target.key);
         }
         if (curr_link.target === d) {
-            active_neighbors.push(curr_link.source);
-        }   
+            in_neighbors.push(curr_link.source);
+            old_link_keys.push(curr_link.source.key + "-" + curr_link.target.key);
+        }
     }
-    
-    var neighbor_num = active_neighbors.length;
-            
+    var in_neighbor_num = in_neighbors.length;
+    var out_neighbor_num = out_neighbors.length;
+    var old_link_num = old_link_keys.length;
+
+    // Add the new nodes and new links
     for (var i = 0; i < total_num; ++i) {
         var datum = node_map[child_keys[i]];
         calculateArcPositions(datum, start_angle, delta, i);
         active_data_nodes.push(datum);
         // Now add the new links
-        for (var j = 0; j < neighbor_num; ++j) {
-            var neighbor = active_neighbors[j];
-            var key_pair = generateKeyForNodeLinkMap(datum, neighbor);
+        for (var j = 0; j < in_neighbor_num; ++j) {
+            var neighbor = in_neighbors[j];
+            var key_pair = neighbor.key + "-" + datum.key;
             var link = node_link_map[key_pair];
             if (link !== undefined) {
                 active_data_links.push(link);
-                console.log(active_data_links);
+            }
+        }
+        for (var j = 0; j < out_neighbor_num; ++j) {
+            var neighbor = out_neighbors[j];
+            var key_pair = datum.key + "-" + neighbor.key;
+            var link = node_link_map[key_pair];
+            if (link !== undefined) {
+                active_data_links.push(link);
             }
         }
     }
-
     // Add the new links and new nodes resulted from the split
     enterNodes();
     enterLinks();
@@ -68,12 +69,13 @@ function expandRegion(d) {
     // links from the data links
     var pos = $.inArray(d, active_data_nodes);
     active_data_nodes.splice(pos, 1);
-    for (var i = 0; i < neighbor_num; ++i) {
+    for (var i = 0; i < old_link_num; ++i) {
         // Remove the links associated with the expanded node
-        var neighbor = active_neighbors[i];
-        key_pair = generateKeyForNodeLinkMap(d, neighbor);
+        var key_pair = old_link_keys[i];
         link = node_link_map[key_pair];
+        console.log(link);
         var pos = $.inArray(link, active_data_links);
+        console.log(pos);
         active_data_links.splice(pos, 1);
     }
 
@@ -89,6 +91,14 @@ function expandRegion(d) {
     svg.selectAll("line")
        .data(active_data_links, function(d) {return d.key;})
        .exit().remove();
+
+    console.log(active_data_links);
+
+}
+
+function linkClick(d) {
+    console.log(d.source);
+    console.log(d.target);
 }
 
 function enterNodes() {
@@ -98,8 +108,6 @@ function enterNodes() {
         .style("fill", 'white')
         .style("stroke", 'gray')
         .attr("d", arcs)
-        .on("mouseover", fade(.1))
-        .on("mouseout", fade(1))
         .on("click", expandRegion);
 
     svg.selectAll("text")
@@ -120,7 +128,8 @@ function enterLinks() {
         .attr("x1", function(d) { return d.source.x; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y1", function(d) { return d.source.y; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("y2", function(d) { return d.target.y; })
+        .on("click", linkClick);
 }
 
 
@@ -139,5 +148,5 @@ function calculateArcPositions(datum, start_angle, delta, i) {
     var angle = delta * (i + 0.5) + start_angle;
     var radius = inner_radius + (outer_radius - inner_radius) / 2;
     datum.x = radius * Math.cos(Math.PI / 2 - angle);
-    datum.y = radius * Math.sin(Math.PI / 2 - angle);
+    datum.y = -radius * Math.sin(Math.PI / 2 - angle);
 }
