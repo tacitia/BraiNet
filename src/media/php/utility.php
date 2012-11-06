d2f8d4a75f7bd87c8e4ab76327e7e000501e3bcc
Yun Miao <yoyomyo@yun-miaos-macbook.local>
2012-11-06 15:08:38 -0500

    Added config.php and utility.php.

---
+++ src/media/php/utility.php
@@ -0,0 +1,15 @@
+<?php
+function getCurrentUri()
+{
+	$pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+	if ($_SERVER["SERVER_PORT"] != "80")
+	{
+		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	} 
+	else 
+	{
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	return $pageURL;
+}
+?>
