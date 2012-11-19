4981a8686e95b2faad5c61cbdaa8baf656c6a605
Yun Miao <yoyomyo@yun-miaos-macbook.local>
2012-11-19 16:13:30 -0500

    user login

--- src/media/php/config.php
+++ src/media/php/config.php
@@ -1,8 +1,9 @@
 <?php
-
-include_once 'utility.php';
-
+$appname = 'Brain Circuits Visualization';
 $client_id='26171650212.apps.googleusercontent.com';
 $client_secret='b_rMSdz5lkVxVQlVat-LS2P3';
-$callback='http://localhost:8888/BViz/BrainCircuitsViz/src/';
+//$callback needs to be changed so that it returns the right url
+//for either production or localhost
+$callback='http://localhost:8888/BViz/BrainCircuitsViz/src/';  
+$developer_key = 'AIzaSyC_ytIywMicvulZHbIwTOz05M8pZVo7FQM';
 ?>
