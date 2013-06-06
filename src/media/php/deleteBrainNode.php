5b6a5c76c9eed7675e0a86ab7e0a244bcd877275
tacitia <lillian.g621@gmail.com>
2013-06-06 14:40:26 -0400

    z

--- src/media/php/deleteBrainNode.php
+++ src/media/php/deleteBrainNode.php
@@ -25,16 +25,13 @@
     //but right now, we just update all immediate child parent and set it to -1
     //also need to delete all links associated with the node
 
-	echo $nodeKey;
-	echo $userID;
-	
 	if ($isClone) {
 		mysql_query("INSERT INTO diff_nodes (nodeKey, diff, userID, origin) VALUES ('$nodeKey', 'Delete', '$userID', '$origin')") or die("an error occurred when deleting cloned node");
 	}
 	else {
     	mysql_query("UPDATE node_parents SET parent = -1 WHERE parent = ".$nodeKey) or die("an error occured when updating parent");
-    	mysql_query("DELETE FROM user_nodes WHERE key = " . $nodeKey) or die("an error occured when deleting node");
     	mysql_query("DELETE FROM user_links WHERE sourceKey = ".$nodeKey." OR targetKey = ".$nodeKey) or die("an error occured when deleting node's links");
+    	mysql_query("DELETE FROM user_nodes WHERE `key` = " . $nodeKey) or die("an error occured when deleting node");
     }
     
     echo mysql_error($con);
