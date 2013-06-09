cc96671e057937603e9b2290688069c620f8b7e4
tacitia <lillian.g621@gmail.com>
2013-06-08 20:52:01 -0400

    fix error in updateBrainNode.php

--- src/media/php/updateBrainNode.php
+++ src/media/php/updateBrainNode.php
@@ -7,27 +7,29 @@
     $origin = $_POST['origin'];
     
     $con = mysql_connect("localhost", "tacitia_brainIDC", "Ophelia621");
+    if (!$con) {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    mysql_select_db("brainconnect_brainData", $con);
+
     $nodeName = mysql_real_escape_string($nodeName);
     $notes = mysql_real_escape_string($notes);
     
     if ($isClone) {
-    	echo "???";
     	if ($nodeName) {
-    		mysql_query("INSERT INTO diff_nodes (nodeKey, diff, userID, origin, content) VALUES ('$nodeKey', 'Rename', '$userID', '$origin', '$nodeName')") or die("an error occurred when updating node name");
+    		mysql_query("INSERT INTO diff_nodes (nodeKey, diff, userID, origin, content) VALUES ('$nodeKey', 'Rename', '$userID', '$origin', '$nodeName')") or die("an error occurred when updating node name" . mysql_error());
     	}
     	if ($notes) {
-			mysql_query("INSERT INTO diff_nodes (nodeKey, diff, userID, origin, content) VALUES ('$nodeKey', 'AddNote', '$userID', '$origin', '$notes')") or die("an error occurred when updating node notes");
+			mysql_query("INSERT INTO diff_nodes (nodeKey, diff, userID, origin, content) VALUES ('$nodeKey', 'AddNote', '$userID', '$origin', '$notes')") or die("an error occurred when updating node notes" . mysql_error());
     	}
     }
     else {
     	if ($nodeName) {
-    		echo "UPDATE `user_nodes` SET `name` = '$nodeName' WHERE `key` = '$nodeKey'";
-    		mysql_query("UPDATE user_nodes SET `name` = '$nodeName' WHERE `key` = '$nodeKey'");
-    		echo mysql_error();
-    		 //or die("an error occurred when updating node name");
+    		mysql_query("UPDATE user_nodes SET `name` = '$nodeName' WHERE `key` = '$nodeKey'") or die("an error occurred when updating node name" . mysql_error());
     	}
     	if ($notes) {
-			mysql_query("UPDATE user_nodes SET `notes` = '$notes' WHERE `key` = '$nodeKey'") or die("an error occurred when updating node notes");
+			mysql_query("UPDATE user_nodes SET `notes` = '$notes' WHERE `key` = '$nodeKey'") or die("an error occurred when updating node notes" . mysql_error());
     	}
     }
     
