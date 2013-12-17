22dd8e873ccdfe6af2e8badd7c3e567006e97b9a
tacitia <lillian.g621@gmail.com>
2013-12-17 17:46:28 +0800

    z

--- BraiNet/connectivity/urls.py
+++ BraiNet/connectivity/urls.py
@@ -4,5 +4,6 @@ from connectivity.views import *
 #TODO: modify it so that getDatasets takes in user_id and dataset_id as parameters
 urlpatterns = patterns('',
 	url(r'^datasets/(?P<user_id>\d+)/$', getDatasets, name='getDatasets'),
+	url(r'^datasets/(?P<user_id>\d+)/connectivity/datasets/(?P<dummy_user_id>\d+)/$', getDatasets, name='getDatasets'),
 	url(r'^dataset/(?P<user_id>\d+)/(?P<dataset_id>\d+)/(?P<max_depth>\d+)$', getDataset, name='getDataset'),
 )
