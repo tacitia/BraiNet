0f77c370ddc090f0c0f35465735ca6b941952d1b
tacitia <lillian.g621@gmail.com>
2014-01-06 21:40:47 +0800

    z

--- BraiNet/connectivity/views.py
+++ BraiNet/connectivity/views.py
@@ -8,14 +8,14 @@ from django.http import HttpResponse
 
 # Get and post connectivity data based on user id and dataset id
 
-def getDatasets(request, user_id):
+def getDatasets(request, user_id, dummy_user_id):
 	user = Account.objects.get(access_code=user_id)
 	datasets = Dataset.objects.filter(Q(visibility='public') | Q(user_id=user.id))
 	response_data = serializers.serialize('json', datasets)
 	return HttpResponse(json.dumps(response_data), content_type='application/json')
 	
 
-def getDataset(request, user_id, dataset_id, max_depth):
+def getDataset(request, user_id, dataset_id, max_depth, dummy_user_id, dummy_dataset_id, dummy_max_depth):
 	# TODO: Check whether the request is legal (i.e. user id matches dataset id)
 #	print Connection.objects.filter(Q(dataset_id=dataset_id) & Q(source_id__depth__lte=max_depth) & Q(target_id__depth__lte=max_depth)).count()
 #	print Structure.objects.filter(Q(dataset_id=dataset_id) & Q(depth__lte=max_depth)).count()
