dc333121c45926d41fad5400f2e9469090bbeccc
tacitia <lillian.g621@gmail.com>
2014-05-26 13:46:44 -0400

    add svg module for legend creation

---
+++ BraiNet/homepage/static/javascript/svg/legend.js
@@ -0,0 +1,38 @@
+// This module 
+// TODO: change this module to require setting.js and amplifyJS.js using requireJS
+
+svg.legend = (function($, undefined) {
+
+	var doms = {
+		canvas: '#legend-primary #legend-canvas'
+	};
+	
+	var localSettings = {
+        vis: {}
+    };
+
+    vis.width = 600;
+    vis.height = 100;
+	
+	var svgObjs = {
+		canvas: null
+	};
+
+	var init = function() {
+		svgObjs.canvas = d3.select(doms.canvas)
+			.style('width', localSettings.vis.width)
+			.style('height', localSettings.vis.height);
+	};
+	
+	var render = function() {
+        svgObjs.canvas.append('svg:line')
+            .attr('stroke-width', 3)
+            .attr('stroke', 'black');
+	};
+
+	return {
+		init: init,
+		render: render
+	};
+
+}(jQuery));
