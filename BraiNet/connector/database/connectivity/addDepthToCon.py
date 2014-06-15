8e28d9d76b903d1f0d73d80bbe1538ba0696c4e3
tacitia <lillian.g621@gmail.com>
2014-06-15 14:22:46 -0400

    modify connector scripts to save memory

--- BraiNet/connector/database/connectivity/addDepthToCon.py
+++ BraiNet/connector/database/connectivity/addDepthToCon.py
@@ -9,18 +9,22 @@ from django.db.models import Q
 super_user = Account.objects.get(access_code='abcdefgh')
 
 for dataset in [1,2]:
-	connections = Connection.objects.filter(dataset_id=dataset)
-	# Populate the source_depth and target_depth fields
-	max_depth = -1
-	id = 0
-	for c in connections.iterator():
-		c.source_depth = c.source_id.depth
-		c.target_depth = c.target_id.depth
-		if c.source_id.depth > max_depth:
-			max_depth = c.source_id.depth
-		if c.target_id.depth > max_depth:
-			max_depth = c.target_id.depth
-		id += 1
-		c.save()
-		print id
-	print max_depth
+	step = 20000
+	for unit in range(0,12):
+		start = step * unit
+		end = step * (unit+1) - 1
+		connections = Connection.objects.filter(dataset_id=dataset)[start:end]
+		# Populate the source_depth and target_depth fields
+		max_depth = -1
+		id = 0
+		for c in connections.iterator():
+			c.source_depth = c.source_id.depth
+			c.target_depth = c.target_id.depth
+			if c.source_id.depth > max_depth:
+				max_depth = c.source_id.depth
+			if c.target_id.depth > max_depth:
+				max_depth = c.target_id.depth
+			id += 1
+			c.save()
+			print id
+		print max_depth
